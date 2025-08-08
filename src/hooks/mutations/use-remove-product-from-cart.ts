ad13abc5a22328bcbf2ebf9473b5713f41fe34bb
JuniorMira0 <j-unyor14@hotmail.com>
2025-08-08 18:47:10 -0300

    feat: refactor cart item and cart components to use custom hooks for product mutations and cart queries

---
+++ src/hooks/mutations/use-remove-product-from-cart.ts
@@ -0,0 +1,17 @@
+import { removeProductFromCart } from "@/actions/remove-cart-product";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getUseCartQueryKey } from "../queries/use-cart";
+
+export const getRemoveCartProductMutationKey = (cartItemId: string) =>
+  ["remove-cart-product", cartItemId] as const;
+
+export const useRemoveProductFromCart = (cartItemId: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: getRemoveCartProductMutationKey(cartItemId),
+    mutationFn: () => removeProductFromCart({ cartItemId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: getUseCartQueryKey() });
+    },
+  });
+};
