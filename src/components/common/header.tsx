9b44607f8ad22716855846e63d77d81f91619ef7
JuniorMira0 <j-unyor14@hotmail.com>
2025-08-12 20:19:24 -0300

    feat: update logo properties in Header component for improved display

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -22,7 +22,13 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-between p-5">
       <Link href="/">
-        <Image src="/logo.png" alt="BEWEAR" width={100} height={26.14} />
+        <Image
+          src="/logo.png"
+          alt="MIRASWEAR"
+          width={200}
+          height={26.14}
+          className="dark:invert-0"
+        />
       </Link>
 
       <div className="flex items-center gap-3">
