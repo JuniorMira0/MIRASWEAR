2dc79b2dbe0e028c43c80f44bbaea751629d624c
JuniorMira0 <j-unyor14@hotmail.com>
2025-08-13 14:58:51 -0300

    refactor(product-variant-page): replace direct database queries with dedicated data retrieval functions

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import { eq } from "drizzle-orm";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
-import { db } from "@/db";
-import { productTable, productVariantTable } from "@/db/schema";
+import {
+  getLikelyProducts,
+  getProductVariantBySlug,
+} from "@/data/products/get-product-variant";
 import { formatCentsToBRL } from "@/helpers/money";
 
 import ProductActions from "./components/product-actions";
@@ -18,25 +19,16 @@ interface ProductVariantPageProps {
 
 const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
   const { slug } = await params;
-  const productVariant = await db.query.productVariantTable.findFirst({
-    where: eq(productVariantTable.slug, slug),
-    with: {
-      product: {
-        with: {
-          variants: true,
-        },
-      },
-    },
-  });
+  const productVariant = await getProductVariantBySlug(slug);
+
   if (!productVariant) {
     return notFound();
   }
-  const likelyProducts = await db.query.productTable.findMany({
-    where: eq(productTable.categoryId, productVariant.product.categoryId),
-    with: {
-      variants: true,
-    },
-  });
+
+  const likelyProducts = await getLikelyProducts(
+    productVariant.product.categoryId,
+    productVariant.product.id,
+  );
   return (
     <>
       <Header />
---
+++ src/data/products/get-product-variant.ts
@@ -0,0 +1,31 @@
+import { db } from "@/db";
+import { productTable, productVariantTable } from "@/db/schema";
+import { and, eq, ne } from "drizzle-orm";
+
+export const getProductVariantBySlug = async (slug: string) => {
+  return await db.query.productVariantTable.findFirst({
+    where: eq(productVariantTable.slug, slug),
+    with: {
+      product: {
+        with: {
+          variants: true,
+        },
+      },
+    },
+  });
+};
+
+export const getLikelyProducts = async (
+  categoryId: string,
+  excludeProductId: string,
+) => {
+  return await db.query.productTable.findMany({
+    where: and(
+      eq(productTable.categoryId, categoryId),
+      ne(productTable.id, excludeProductId),
+    ),
+    with: {
+      variants: true,
+    },
+  });
+};
