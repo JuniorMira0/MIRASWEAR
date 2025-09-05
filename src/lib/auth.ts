a6cae118aa27cbe667d2f3ae95b3978d09c3588d
JuniorMira0 <j-unyor14@hotmail.com>
2025-09-05 16:38:55 -0300

    feat(authentication): remove Google sign-in functionality from the sign-in form

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,12 +15,6 @@ const authDb = drizzle(authPool, {
 });
 
 export const auth = betterAuth({
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
   database: drizzleAdapter(authDb, {
     provider: "pg",
     schema: {
